a23740ad17d94fab41e4c968aa93c3d32938dbe7
Zeljko Tepsic <ztepsic@gmail.com>
2018-10-26 01:14:04 +0200

    Added description to assembly info

--- TvMaze/Properties/AssemblyInfo.cs
+++ TvMaze/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TvMaze")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The TvMaze .NET Client")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ztepsic")]
 [assembly: AssemblyProduct("TvMaze")]
